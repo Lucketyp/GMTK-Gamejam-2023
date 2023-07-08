a2cd6c2cdbe46a1d71f8d0e31d6832f05913fd78
Lucketyp <lucke_johnson@yahoo.se>
2023-07-08 23:59:31 +0200

    restart meny

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-
+    [SerializeField] GameObject RestartScreen;
+    [SerializeField] GameObject BearModel;
     [SerializeField] LayerMask deadlyLayers;
     Vector3 initalPosition;
 
@@ -13,16 +14,23 @@ public class Player : MonoBehaviour
         initalPosition = transform.position;
     }
 
-    void Update()
-    {
-        
-    }
-
     void OnTriggerEnter(Collider other) {
         Debug.Log(other.gameObject.layer);
         if(deadlyLayers == (deadlyLayers | (1 << other.gameObject.layer))){
-            //transform.position = initalPosition;
-            Debug.Log("Dieded");
+            StartCoroutine(PlayerDeath());
         }
+    }
+
+    IEnumerator PlayerDeath() {
+        GetComponentInChildren<Renderer>().material.color = Color.red;
+        // stop the player from moving
+        GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+        // rotate the player 90 degrees slowly
+        for(int i = 0; i < 90; i++){
+            BearModel.transform.Rotate(0, 0, 1);
+            BearModel.transform.position += new Vector3(0, 0.005f, 0);
+            yield return new WaitForSeconds(0.01f);
+        }
+        RestartScreen.SetActive(true);
     }
 }
