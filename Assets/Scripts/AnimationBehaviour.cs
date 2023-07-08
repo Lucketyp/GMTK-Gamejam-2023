e84227d35e3eb5085620c65e3c9c815bb89e09ff
Lucketyp <lucke_johnson@yahoo.se>
2023-07-08 15:19:26 +0200

    inte perfekt animation

--- Assets/Scripts/AnimationBehaviour.cs
+++ Assets/Scripts/AnimationBehaviour.cs
@@ -7,6 +7,7 @@ public class AnimationBehaviour : MonoBehaviour
     Animator animator;
     Rigidbody rb;
     float speed;
+
     void Start()
     {
         animator = GetComponent<Animator>();
@@ -17,5 +18,12 @@ public class AnimationBehaviour : MonoBehaviour
     {
         speed = rb.velocity.magnitude;
         animator.SetFloat("Speed", speed);
+
+        //Face the direction of movement
+        Vector3 direction = -rb.velocity;
+        direction.y = 0;
+        if(direction.magnitude > 0.1){
+            transform.rotation = Quaternion.LookRotation(direction);
+        }
     }
 }
