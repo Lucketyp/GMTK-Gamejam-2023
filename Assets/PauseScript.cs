925fb5713f8e9a688e381efb4e3ed9d2cf6ef528
Lucketyp <lucke_johnson@yahoo.se>
2023-07-09 19:20:59 +0200

    pause menu

---
+++ Assets/PauseScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseScript : MonoBehaviour
+{
+
+    bool paused = false;
+    [SerializeField] GameObject pauseMenu;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            paused = !paused;
+            Time.timeScale = paused ? 0 : 1;
+            pauseMenu.SetActive(paused);
+            //Set all audio sources to paused
+            if (paused) {
+                foreach(AudioSource audioSource in FindObjectsOfType<AudioSource>()){
+                    audioSource.Pause();
+                }
+            } else {
+                foreach(AudioSource audioSource in FindObjectsOfType<AudioSource>()){
+                    audioSource.UnPause();
+                }
+            }
+        }
+    }
+}
