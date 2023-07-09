5179a302f22fc34ff746d6495365d270caa74906
Lucketyp <lucke_johnson@yahoo.se>
2023-07-09 14:29:31 +0200

    traptraptrap

--- Assets/Trap.cs
+++ Assets/Trap.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 public class Trap : MonoBehaviour
 {
     Animator animator;
+    AudioSource audioSource;
     void Start()
     {
-        animator = GetComponent<Animator>();
+        animator = GetComponentInChildren<Animator>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     void OnTriggerEnter(Collider other) {
         if(other.tag == "Player") {
+            animator.SetTrigger("Activate");
+            audioSource.Play();
             other.GetComponentInParent<Player>().TakeDamage(2);
         }
     }
